5d5f3aabac360e4957921bf62ac8357b7f147d5e
Sai Ko <saiko@sais-mbp.home>
2023-12-08 15:26:55 -0500

    done basic oop of dart 🚀

--- program.dart
+++ program.dart
@@ -1,31 +1,41 @@
-// creating an interface using concrete class
-class Laptop {
-    // method
-//   turnOn() {
-//     print('Laptop turned on');
-//   }
-    // method
-  turnOff() {
-    print('Laptop turned off');
-  }
-}
+class Person {
+  final String name;
 
-class MacBook implements Laptop {
-  // implementation of turnOn()
-  @override
-  turnOn() {
-    print('MacBook turned on');
-  }
+  // Private constructor
+  Person._internal(this.name);
 
-  // implementation of turnOff()
-  @override
-  turnOff() {
-    print('MacBook turned off');
+  // Static cache field as a list
+  static final List<Person> _cache = [];
+
+  // Factory constructor
+  factory Person(String name) {
+    print(_cache);
+    bool isNameIncluded = _cache.any((person) => person.name == name);
+    if (isNameIncluded) {
+     return _cache.first;
+    } else {
+      final person = Person._internal(name);
+      _cache.add(person);
+      return person;
+    }
   }
 }
 
 void main() {
-  var macBook = MacBook();
-  macBook.turnOn();
-  macBook.turnOff();
+  // Creating instances using the factory constructor
+  final person1 = Person('John');
+  final person2 = Person('Harry');
+  final person3 = Person('John');
+
+  // Instances with the same name share the same instance in memory
+  print(
+      "Person1 name is : ${person1.name} with identity ${identityHashCode(person1)}");
+  print(
+      "Person2 name is : ${person2.name} with identity ${identityHashCode(person2)}");
+  print(
+      "Person3 name is : ${person3.name} with identity ${identityHashCode(person3)}");
 }
+
+// Helper function to print identity hashcode
+int identityHashCode(Object obj) => obj.hashCode;
+
